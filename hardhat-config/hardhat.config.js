a0faadce0f411d786e81d466b14e0f856ccd4cd9
Luong Minh <ngocminhluong182@gmail.com>
2024-04-08 11:59:01 +0700

    update hardhat config and version

--- hardhat-config/hardhat.config.js
+++ hardhat-config/hardhat.config.js
@@ -2,9 +2,17 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.21",
   networks: {
     hardhat: {
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: "",
+      },
+
       chainId: 1337,
       mining: { // Mines a new block every .5 seconds
         auto: false,
